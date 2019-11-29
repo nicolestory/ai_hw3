349b9b324e76fca9f3b2ba102277c990e1e60f30
Nicole Story <nicole7story@gmail.com>
2019-11-29 16:53:26 -0600

    Updates to printing/saving

--- problem_3_update_rule.py
+++ problem_3_update_rule.py
@@ -25,7 +25,7 @@ def main():
     times = []
     predictions = []
     estimate = 0.0
-    print('Points:')
+    print('\nPoints:')
 
     for time in range(len(signals)):
         times.append(time)
@@ -33,16 +33,14 @@ def main():
         estimate = estimate + alpha(time) * (signals[time] - estimate)
         print('({}, {})'.format(times[time], predictions[time]))
 
-    print(times)
-    print(predictions)
-
     error = 0
     for time in range(10, 16):
         error += abs(signals[time] - predictions[time])
-        print(signals[time], predictions[time])
 
+    print('\nError in predictions 10-15:')
     print('Total error: {}, avg error: {}'.format(error, error / 6))
 
+    pyplot.title('Signal prediction with step size {}'.format(step_size_string))
     pyplot.plot(times, predictions, marker='o')
     pyplot.plot(times, signals)
     pyplot.grid()
@@ -69,7 +67,7 @@ if __name__ == '__main__':
     elif problem_num == 5:
         step_size_string = '1.5'
     else:
-        step_size_string = 0
+        step_size_string = '0.0'
     print('Using step size {}'.format(step_size_string))
 
     main()
